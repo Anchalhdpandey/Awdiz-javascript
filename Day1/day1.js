549fcf4cf91926416937da85881d2d77177ea340
Anchalhdpandey <152679483+Anchalhdpandey@users.noreply.github.com>
2024-01-28 13:19:57 +0530

    updated

---
+++ Day1/day1.js
@@ -0,0 +1,46 @@
+console.log("Awdiz")
+console.log(typeof("pass@123"))
+console.log('Awdiz')
+console.log(typeof('true'))
+console.log(`awdiz`)
+console.log(typeof(`123.78`))
+console.log("rffvjf", typeof("fgrfj"), "763387468", "bgbgbgnbg")
+console.log(2+2)
+console.log(1234, typeof(1234))
+console.log("1234", typeof("1234"))
+console.log(1+1)
+console.log("1" +'1')
+console.log(true, typeof(true))
+console.log(false, typeof(false))
+console.log("true", typeof("true"))
+var instituteName="awdiz";
+var age=23;
+var isOpen =true
+console.log(instituteName, age, isOpen)
+var value1=10;
+var value2=28;
+console.log(value1+value2)
+console.log(value1-value2)
+console.log(value1*value2)
+console.log(value1/value2)
+var kuchbhi=12;
+console.log(kuchbhi)
+console.log(kuchbhi)
+console.log(kuchbhi)
+console.log(kuchbhi)
+var myValue=10
+console.log(myValue)
+myValue=20
+console.log(myValue)
+myValue=myValue+10
+console.log(myValue)
+// var myValue=50;
+// console.log(myValue)
+// const myName=1234;
+// myName=654;
+// const myName=342546;
+// console.log(myName)
+// let myFav=1234;
+// myFav=654;
+// let myFav=342546;
+// console.log(myFav)
