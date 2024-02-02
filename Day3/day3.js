2ce9bf01509c7aae07d496cbb883bdd95b991242
Anchalhdpandey <152679483+Anchalhdpandey@users.noreply.github.com>
2024-02-02 22:39:20 +0530

    updates

---
+++ Day3/day3.js
@@ -0,0 +1,23 @@
+if(5>3){
+    console.log("condition is true")
+}else{
+    console.log("condition is false")
+}
+var value1=31;
+var value2=20;
+var value3=30;
+if(value1>value2){
+    console.log("value1 is greater than value2")
+}else if(value1>value3){
+    console.log("value1 is bigger than value3")
+}else{
+    console.log("value1 is lesser than value2 and value3")
+}
+var age=22;
+if(age<18){
+    console.log("You are eligible for license")
+}else if(age>=18 && age<80){
+    console.log('you are eligible for license')
+}else if(age>80){
+    console.log("Not eligible for license")
+}
