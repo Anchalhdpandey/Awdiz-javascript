58e854aa4508314ae4658f422a50385772237d2f
Anchalhdpandey <152679483+Anchalhdpandey@users.noreply.github.com>
2024-02-15 23:16:46 +0530

    up

---
+++ Day10/day10.js
@@ -0,0 +1,14 @@
+var array=[1,2,3,4,5,6,7,8,9]
+const res=array.push(6)
+console.log(res,"res")
+console.log(array);
+var array1=[1,2,3]
+array1.unshift(5)
+console.log(array1)
+array1.unshift(4)
+console.log(array1)
+var array2=[1,2,3,5]
+const deleted=array2.shift
+array2.shift()
+console.log(array2)
+const array3=['a','b','c']
---
+++ Practice/prac.js
@@ -0,0 +1,28 @@
+// // function declaration
+// function singHappyBirthday(){
+//     console.log("Happy birthday to you......");
+// }
+// singHappyBirthday()
+// // Adding parameters
+// function sumThreeNumbers(num1, num2, num3){
+//     return num1+num2+num3;
+// }
+
+// const number = sumThreeNumbers(44,88,3314);
+// console.log(number)
+// function findTarget(array,target){
+//     for(i=0;i<array.length;i++){
+//         if(array[i]==target){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+// const myArray=[1,2,3,4,5,6,7]
+// const ans = findTarget(myArray,6)
+// console.log(ans)
+
+// function expression
+const number = function (par1, par2, par3) {
+  return par1 * par2 * par3;
+};
